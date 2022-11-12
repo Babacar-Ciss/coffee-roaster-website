562361424b6c16582da941952adbb29e6ce864af
Babacar Ciss <kratosnvidia@gmail.com>
2022-11-12 17:19:36 +0000

    setting hamburger button clickable

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import Link from 'next/link'
+import useStore from '../store'
 
 const MenuStyle = styled.div`
   position: fixed;
@@ -37,14 +38,18 @@ const LinksContainer = styled.div`
 `
 
 const Menu = () => {
+  const isHamburgerClicked = useStore((state) => state.isHamburgerClicked)
+
   return (
-    <MenuStyle>
-      <LinksContainer>
-        <Link href="/">Home</Link>
-        <Link href="/About_Us">About Us</Link>
-        <Link href="/Create_Your_Plan">Create Your Plan</Link>
-      </LinksContainer>
-    </MenuStyle>
+    isHamburgerClicked && (
+      <MenuStyle>
+        <LinksContainer>
+          <Link href="/">Home</Link>
+          <Link href="/About Us">About Us</Link>
+          <Link href="/Create Your Plan">Create Your Plan</Link>
+        </LinksContainer>
+      </MenuStyle>
+    )
   )
 }
 
