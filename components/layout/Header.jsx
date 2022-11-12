e8df83db262a5f731addc34c5d5d70fec7bf820d
Babacar Ciss <kratosnvidia@gmail.com>
2022-11-12 15:25:21 +0000

    adding zustand as a state manager

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,20 +1,32 @@
-import styled from "styled-components";
-import Image from "next/image";
-import Link from "next/link"
-
+import styled from 'styled-components'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const HeaderStyle = styled.div`
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 `
 
 const Header = () => {
-    return (
-        <HeaderStyle>
-            <Link href="/"><Image src="/assets/shared/desktop/logo.svg" alt="roasted coffee logo" width="163" height="18"/></Link>     
-            <Image style={{cursor : "pointer"}} src="/assets/shared/mobile/icon-hamburger.svg" alt="roasted coffee logo" width="16" height="15"/>
-        </HeaderStyle>
-    );
+  return (
+    <HeaderStyle>
+      <Link href="/">
+        <Image
+          src="/assets/shared/desktop/logo.svg"
+          alt="roasted coffee logo"
+          width="163"
+          height="18"
+        />
+      </Link>
+      <Image
+        style={{ cursor: 'pointer' }}
+        src="/assets/shared/mobile/icon-hamburger.svg"
+        alt="roasted coffee logo"
+        width="16"
+        height="15"
+      />
+    </HeaderStyle>
+  )
 }
 
-export default Header;
+export default Header
